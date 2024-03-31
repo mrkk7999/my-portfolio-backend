4303b4e67d06dc23caa6709400f2551b160fc8ae
kk79cpd <kirank@copperpoddigital.com>
2024-03-31 15:24:04 +0530

    /health-check added

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/mrkk7999/my-portfolio-backend
 
 go 1.22.0
+
+require github.com/gorilla/mux v1.8.1 // indirect
--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func healthCheckHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Working Fine!"))
+}
 
 func main() {
-	fmt.Println("Printing from portfolio backend")
+	router := mux.NewRouter()
+
+	router.HandleFunc("/health-check", healthCheckHandler).Methods("GET")
+
+	fmt.Println("Server started Listening on :8000")
+	fmt.Println("health-check api :", "http://localhost:8000/health-check")
+	http.ListenAndServe(":8000", router)
 }
